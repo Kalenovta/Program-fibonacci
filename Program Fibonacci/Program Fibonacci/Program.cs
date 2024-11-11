f9d94a5c39d58298f7a2ed06b688dff265eb4fff
Kalenovta <Kalenovtakhoirulanwar@gmail.com>
2024-11-11 12:44:01 +0700

    Add files via upload

---
+++ Program Fibonacci/Program Fibonacci/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Minta user memasukkan jumlah elemen Fibonacci yang ingin dihitung
+        Console.Write("Masukkan jumlah elemen Fibonacci: ");
+        int n = int.Parse(Console.ReadLine());
+
+        // Memanggil fungsi Fibonacci untuk menampilkan hasil
+        Fibonacci(n);
+    }
+
+    static void Fibonacci(int n)
+    {
+        int firstNumber = 0, secondNumber = 1, nextNumber;
+
+        // Jika n = 1, hanya cetak elemen pertama
+        if (n == 1)
+        {
+            Console.WriteLine("Fibonacci: " + firstNumber);
+        }
+        else
+        {
+            Console.Write("Fibonacci: " + firstNumber + " " + secondNumber + " ");
+
+            // Hitung elemen Fibonacci berikutnya hingga mencapai n elemen
+            for (int i = 2; i < n; i++)
+            {
+                nextNumber = firstNumber + secondNumber;
+                Console.Write(nextNumber + " ");
+
+                // Update nilai untuk dua elemen sebelumnya
+                firstNumber = secondNumber;
+                secondNumber = nextNumber;
+            }
+        }
+        Console.WriteLine(); // Pindah ke baris berikutnya setelah selesai mencetak
+    }
+}
+
